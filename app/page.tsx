7d184c18f4eafbd9d0d7b8e0902ab44308420780
Melike Rana Yozgatlı <153318498+mlkyzgt@users.noreply.github.com>
2024-03-24 16:16:55 +0300

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Navbar from "./components/navbar";
 import Main from "./components/main";
+import "./page.css";
+
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Main />
+    <div className="page">
+      <div>
+        <Navbar />
+        <Main />
+      </div>
     </div>
   );
 }
